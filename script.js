262db0ad235eaa179e79ecfe4e095a95a6a74fa7
Bohdan Husiev <bgusev77@gmail.com>
2023-06-13 23:21:28 +0300

    qwerty

---
+++ script.js
@@ -0,0 +1,10 @@
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+let key = 0;
+const generateKey = (a, characters) => {
+  for (let i = 0; i < a; i++) {
+    key += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return key;
+};
+
+console.log(generateKey(16, characters));
